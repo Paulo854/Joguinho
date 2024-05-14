e7557c9b9469a7762474a38c4e5c649267a40476
Triz-Hara <168754092+Triz-Hara@users.noreply.github.com>
2024-05-14 19:23:01 -0300

    Update Protagonista.java

--- src/jogoA3/Protagonista.java
+++ src/jogoA3/Protagonista.java
@@ -1,7 +1,8 @@
+package jogoA3;
+
 import java. util.Random;
 public class Protagonista extends Personagem{
 
-private double dano;
 
 public Protagonista(int maxVida, int vida, int ataque) {
 
@@ -18,9 +19,9 @@ return super.getDefesa();
 
 public void atacar(){
 
-//Construtor para gerar números aleatórios
+//Construtor para gerar nÃºmeros aleatÃ³rios
 Random r = new Random();
-//Variável de controle
+//VariÃ¡vel de controle
 int i;
 
 i=r.nextInt(10);
@@ -28,27 +29,28 @@ i=r.nextInt(10);
 //10% de chance de errar
 if (i<2){
 
-dano=0;
+super.setDano(0);
 }
 
-// 30% chance de ataque crítico
+// 30% chance de ataque crÃ­tico
 else if (i>7){
 
-dano=super.getAtaque()*1.1;
+super.setDano(super.getAtaque()*1.1);
 }
 
 //60% chance de ataque normal
 else{
 
-dano=super.getAtaque();
+super.setDano(super.getAtaque());
 }
 
 }
 
 public void defender(){
 
-dano=0;
+super.setDano(0);
 super.setDefesa(true);
 }
 
+}
 }
