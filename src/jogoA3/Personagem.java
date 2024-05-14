5d7e0fcd0f2c4171a364b41b066d049f7b164bb8
Triz-Hara <168754092+Triz-Hara@users.noreply.github.com>
2024-05-14 19:22:10 -0300

    Update Personagem.java

--- src/jogoA3/Personagem.java
+++ src/jogoA3/Personagem.java
@@ -1,15 +1,18 @@
+package jogoA3;
+
 //CLASSE CONTROLE
 
 public abstract class Personagem{
 
-private int maxVida, vida, ataque;
+private int maxVida, ataque;
 private boolean defesa;
+private double dano, vida;
 
 //SETTERS
 public void setMaxVida(int maxVida){
   this.maxVida=maxVida;
 }
-public void setVida(int vida){
+public void setVida(double vida){
   this.vida=vida;
 }
 public void setAtaque(int ataque){
@@ -18,22 +21,33 @@ public void setAtaque(int ataque){
 public void setDefesa(boolean defesa){
   this.defesa=defesa;
 }
+public void setDano(double dano) {
+	this.dano=dano;
+}
 
 //GETTERS  
 public int getMaxVida(){
   return maxVida;
 }
 public int getVida(){
-  return vida;
-}
+	 int vidaI = (int)vida;
+	return vidaI;
+	}
 public int getAtaque(){
   return ataque;
 }
 public boolean getDefesa(){
   return defesa;
 }
-public double getDano(){
-  return dano;
+public double getDano() {
+int danoI = (int)dano;
+return danoI;
+}
+
+//MÃ‰TODOS ABSTRATOS
+public abstract void atacar();
+public abstract void defender();
+
 }
 
 //MÉTODOS ABSTRATOS
